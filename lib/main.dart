3f49b1dbca98490625b32c2a3584d1823a002e24
wayne <jun_wei_like1@163.com>
2018-12-05 17:33:56 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+        // counter didn't reset back to zero; the application is not restarted.
+        primarySwatch: Colors.green,
+      ),
+//      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new RandomWords()
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget{
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+
+  final _saved = new Set<WordPair>();
+
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions(){
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return new Divider();
+
+          final index = i ~/ 2;
+
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        }
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+//    添加 alreadySaved来检查确保单词对还没有添加到收藏夹中。
+    final alreadySaved = _saved.contains(pair);
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+//    final wordPair = new WordPair.random();
+//    return new Text(wordPair.asPascalCase);
+  return new Scaffold(
+    appBar: new AppBar(
+      title: new Text('Startup Name Generator'),
+      actions: <Widget>[
+        new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
+      ],
+    ),
+    body: _buildSuggestions(),
+  );
+  }
+
+
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+          builder: (context) {
+            final tiles = _saved.map(
+                (pair) {
+                  return new ListTile(
+                    title: new Text(
+                      pair.asPascalCase,
+                      style: _biggerFont,
+                    ),
+                  );
+                }
+            );
+            final divided = ListTile
+            .divideTiles(
+              context: context,
+              tiles: tiles,
+            ).toList();
+
+            return new Scaffold(
+              appBar: new AppBar(
+                title: new Text('Saved Suggestions'),
+              ),
+              body: new ListView(children: divided),
+            );
+          }
+      ),
+    );
+  }
+}
