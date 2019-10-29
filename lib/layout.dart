9185415d927bf48279b153ae58f1c6d354ab731f
wayne <jun_wei_like1@163.com>
2019-10-29 16:23:37 +0800

    flutter 布局学习

---
+++ lib/layout.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+class LayoutPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    Widget titleSection = new Container(
+      padding: const EdgeInsets.all(32.0),
+      child: new Row(
+        children: <Widget>[
+          new Expanded(
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new Container(
+                    padding: const EdgeInsets.only(bottom: 0.0),
+                    child: new Text(
+                      'Oeschinen Lake Campground',
+                      style: new TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  new Text(
+                    'Kandersteg, Switzerland',
+                    style: new TextStyle(
+                      color: Colors.grey[500]
+                    ),
+                  )
+                ],
+              )
+          ),
+          new Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          new Text('41')
+        ],
+      )
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _bulidButtonColumn(color, Icons.call, 'CALL'),
+          _bulidButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _bulidButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+            'Alps. Situated 1,578 meters above sea level, it is one of the '
+            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+            'half-hour walk through pastures and pine forest, leads you to the '
+            'lake, which warms to 20 degrees Celsius in the summer. Activities '
+            'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter layout demo'),
+        ),
+        body: ListView(
+          children: <Widget>[
+            Image.asset('images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection
+          ],
+        ),
+      )
+    );
+  }
+
+  Column _bulidButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'layout.dart';
 
 void main() => runApp(MyApp());
 
@@ -27,10 +28,12 @@ class MyApp extends StatelessWidget {
 }
 
 class RandomWords extends StatefulWidget{
+  @override
   createState() => new RandomWordsState();
 }
 
 class RandomWordsState extends State<RandomWords> {
+  // 在Dart语言中使用下划线前缀标识符，会强制其变成私有的。
   final _suggestions = <WordPair>[];
 
   final _saved = new Set<WordPair>();
@@ -40,12 +43,19 @@ class RandomWordsState extends State<RandomWords> {
   Widget _buildSuggestions(){
     return new ListView.builder(
         padding: const EdgeInsets.all(16.0),
+        // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
+        // 在偶数行，该函数会为单词对添加一个ListTile row.
+        // 在奇数行，该函数会添加一个分割线widget，来分隔相邻的词对。
+        // 注意，在小屏幕上，分割线看起来可能比较吃力。
         itemBuilder: (context, i) {
+          // 在每一列之前，添加一个1像素高的分隔线widget
           if (i.isOdd) return new Divider();
-
+          // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
+          // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
           final index = i ~/ 2;
-
+          // 如果是建议列表中最后一个单词对
           if (index >= _suggestions.length) {
+            // ...接着再生成10个单词对，然后添加到建议列表
             _suggestions.addAll(generateWordPairs().take(10));
           }
           return _buildRow(_suggestions[index]);
@@ -122,12 +132,7 @@ class RandomWordsState extends State<RandomWords> {
               tiles: tiles,
             ).toList();
 
-            return new Scaffold(
-              appBar: new AppBar(
-                title: new Text('Saved Suggestions'),
-              ),
-              body: new ListView(children: divided),
-            );
+            return LayoutPage();
           }
       ),
     );
