d11aecd6242cbf56f3b37e4f17c3ca4b1b344e2a
wayne <jun_wei_like1@163.com>
2020-01-07 15:53:44 +0800

    flutter listview 上拉加载下载刷新

--- lib/main.dart
+++ lib/main.dart
@@ -60,26 +60,90 @@ class RandomWordsState extends State<RandomWords> {
 
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
-        // 在偶数行，该函数会为单词对添加一个ListTile row.
-        // 在奇数行，该函数会添加一个分割线widget，来分隔相邻的词对。
-        // 注意，在小屏幕上，分割线看起来可能比较吃力。
-        itemBuilder: (context, i) {
-          // 在每一列之前，添加一个1像素高的分隔线widget
-          if (i.isOdd) return new Divider();
-          // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
-          // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
-          final index = i ~/ 2;
-          // 如果是建议列表中最后一个单词对
-          if (index >= _suggestions.length) {
-            // ...接着再生成10个单词对，然后添加到建议列表
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
+  ScrollController _scrollController = new ScrollController();
+
+  bool isLoadMore = false;
+
+  @override
+  void initState() {
+    super.initState();
+//    print(_suggestions.length);
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _loadMore();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  Future<Null> _onRefresh() async {
+    await Future.delayed(Duration(seconds: 1), () {
+      setState(() {
+        _suggestions.clear();
+        _suggestions.addAll(generateWordPairs().take(10));
+      });
+    });
+  }
+
+  Future<Null> _loadMore() async {
+    if (!isLoadMore) {
+      setState(()=> isLoadMore = true);
+      await Future.delayed(Duration(seconds: 1), () {
+        setState(() {
+          _suggestions.addAll(generateWordPairs().take(10));
+          isLoadMore = false;
         });
+      });
+    }
+
+  }
+
+  Widget _buildLoadingIndicator() {
+    return new Padding(padding: const EdgeInsets.all(8.0),
+      child: new Center(
+        child: new Opacity(
+          opacity: isLoadMore ? 1.0 : 0.0,
+          child: new CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return Container(
+      child: RefreshIndicator(
+        child: ListView.builder(
+            itemCount: _suggestions.length + 1,
+            padding: const EdgeInsets.all(16.0),
+            // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
+            // 在偶数行，该函数会为单词对添加一个ListTile row.
+            // 在奇数行，该函数会添加一个分割线widget，来分隔相邻的词对。
+            // 注意，在小屏幕上，分割线看起来可能比较吃力。
+            itemBuilder: (context, i) {
+              // 在每一列之前，添加一个1像素高的分隔线widget
+              if (i.isOdd) return new Divider();
+              // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
+              // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
+              // 如果是建议列表中最后一个单词对
+//              print(_suggestions.length);
+              if (i ==  _suggestions.length){
+                return _buildLoadingIndicator();
+              } else {
+                return _buildRow(_suggestions[i]);
+              }
+            },
+            controller: _scrollController,
+            physics: const AlwaysScrollableScrollPhysics(),
+            ),
+        onRefresh: _onRefresh,
+      ),
+    );
   }
 
   Widget _buildRow(WordPair pair) {
