6d11a3d999fbf490b3352b5cc07e31e9a1efee33
wangjunwei <wangjunwei2357@foryou56.com>
2021-10-09 17:48:06 +0800

    fish_redux:广播通信

--- lib/fish_redux_demo/list_page/action.dart
+++ lib/fish_redux_demo/list_page/action.dart
@@ -1,10 +1,16 @@
 import 'package:fish_redux/fish_redux.dart';
 
 //TODO replace with your own action
-enum ListAction { action, updateItem }
+enum ListAction { action, updateItem, toNotify }
 
 class ListActionCreator {
   static Action updateItem() {
     return Action(ListAction.updateItem);
   }
+
+  /// 广播通知
+  static Action toNotify(String msg) {
+    return Action(ListAction.toNotify, payload: msg);
+  }
+
 }
--- lib/fish_redux_demo/list_page/effect.dart
+++ lib/fish_redux_demo/list_page/effect.dart
@@ -11,8 +11,21 @@ import 'state.dart';
 Effect<ListState> buildEffect() {
   return combineEffects(<Object, Effect<ListState>>{
     Lifecycle.initState: _init,
+    ListAction.toNotify: _toNotify,
   });
 }
+
+///发送广播
+void _toNotify(Action action, Context<ListState> ctx){
+  ///广播通信
+  ctx.broadcast(ListActionCreator.toNotify("广播内容"));
+}
+
+void _receiveNotify(Action action, Context<ListState> ctx) async {
+  ///接受广播
+  print("跳转一页面:${action.payload}");
+}
+
 
 void _init(Action action, Context<ListState> ctx) async{
   String apiUrl = "https://www.wanandroid.com/project/list/1/json";
